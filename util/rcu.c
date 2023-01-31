3fcc4e807cce6057d62b442c58ae56885d5f9d7d
wenhui.zhang <wenhui@gwmail.gwu.edu>
2023-01-31 13:46:14 +0800

    add config

--- util/rcu.c
+++ util/rcu.c
@@ -32,9 +32,6 @@
 #include "qemu/thread.h"
 #include "qemu/main-loop.h"
 #include "qemu/lockable.h"
-#if defined(CONFIG_MALLOC_TRIM)
-#include <malloc.h>
-#endif
 
 /*
  * Global grace period counter.  Bit 0 is always one in rcu_gp_ctr.
@@ -255,9 +252,6 @@ static void *call_rcu_thread(void *opaque)
                 qemu_event_reset(&rcu_call_ready_event);
                 n = qatomic_read(&rcu_call_count);
                 if (n == 0) {
-#if defined(CONFIG_MALLOC_TRIM)
-                    malloc_trim(4 * 1024 * 1024);
-#endif
                     qemu_event_wait(&rcu_call_ready_event);
                 }
             }
